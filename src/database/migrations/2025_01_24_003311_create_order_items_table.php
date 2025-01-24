827f348e4198bb6c9c200f13cccb89a2de2cb632
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-01-24 04:24:19 +0300

    Add 'order_items' model and migration files

---
+++ src/database/migrations/2025_01_24_003311_create_order_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id()->primary()->autoIncrement();
+            $table->integer('quantity');
+            $table->float('unit_prices');
+            $table->float('total');
+            $table->foreignId('order_id');
+            $table->foreignId('product_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
