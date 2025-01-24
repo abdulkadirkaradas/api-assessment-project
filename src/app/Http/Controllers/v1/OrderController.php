272c82f2c64b55d5ff168ca05d019ad5acaaa151
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-01-24 06:13:38 +0300

    Add OrderController file. Add store API function.

---
+++ src/app/Http/Controllers/v1/OrderController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Helpers\CommonFunctions;
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Validators\StoreOrderValidation;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validated = CommonFunctions::validateRequest($request, StoreOrderValidation::class);
+
+        if (isset($validated['status']) && $validated['status'] === BAD_REQUEST) {
+            CommonFunctions::response(BAD_REQUEST, BAD_REQUEST_MSG);
+            return response()->json($validated);
+        }
+
+        $product = Product::find($validated['productId']);
+
+        $unitPrice = $validated['quantity'] * $product->price;
+
+        $newOrder = new Order();
+        $newOrder->customer_id = $validated['customerId'];
+        $newOrder->total = $unitPrice;
+
+        if ($newOrder->save()) {
+            $orderItem = new OrderItem();
+            $orderItem->quantity = $validated['quantity'];
+            $orderItem->unit_price = $product->price;
+            $orderItem->total = $unitPrice;
+            $orderItem->product_id = $validated['productId'];
+
+            if ($newOrder->orderItems()->save($orderItem)) {
+                CommonFunctions::response(SUCCESS, ORDER_CREATED);
+            } else {
+                CommonFunctions::response(FAIL, ORDER_CREATION_FAILED);
+            }
+        } else {
+            CommonFunctions::response(FAIL, ORDER_CREATION_FAILED);
+        }
+    }
+}
