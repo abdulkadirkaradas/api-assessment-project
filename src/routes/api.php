73228184beb6fa388d3946f84555eb79827c1807
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-01-24 06:34:31 +0300

    Add route of order/delete API function

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('/orders', [OrderController::class, 'store']);
+    Route::delete('/order/{id}', [OrderController::class, 'delete']);
 });
