3d065ffe376e6e2f84c0d41aec54797d5baa29c2
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-01-24 06:33:57 +0300

    Add order create, failed and not found constants

--- src/app/Helpers/constants.php
+++ src/app/Helpers/constants.php
@@ -2,5 +2,8 @@
 
 const BAD_REQUEST_MSG = "The request could not be understood or was missing required parameters.";
 const VALIDATOR_FAILED = "The request could not be understood or was missing required parameters.";
+const ORDER_NOT_FOUND = "The order could not be found!";
 const ORDER_CREATION_FAILED = "The order could not be created!";
 const ORDER_CREATED = "The order has been created.";
+const ORDER_DELETE_FAILED = "The order could not be deleted!";
+const ORDER_DELETED = "The order has been deleted.";
