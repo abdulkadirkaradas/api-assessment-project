a84ce199b2c51d052b847f686326ff827442c89a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-01-24 04:24:07 +0300

    Add 'orders' model and migration files

---
+++ src/app/Models/Order.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    protected $autoIncrement = true;
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'total',
+        'order_items_id'
+    ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function orderDiscounts(): HasMany
+    {
+        return $this->hasMany(OrderDiscount::class);
+    }
+}
