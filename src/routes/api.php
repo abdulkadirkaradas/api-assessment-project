72b708bd2b17b92780649404c517542e04f754e0
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-01-24 06:14:21 +0300

    Add API route of 'order/store' API function

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\v1\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::post('/orders', [OrderController::class, 'store']);
+});
